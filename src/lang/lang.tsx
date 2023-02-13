da0bc473dc4a94f35126aa29288d18b7dcb3a631
David Rivera Morales <driveramorales46@gmail.com>
2023-02-12 21:25:41 -0600

    first commit

---
+++ src/lang/lang.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+const dictionaries: Record<string, () => Promise<Record<string, string>>> = {
+  en: () => import("./en.json").then((module) => module.default),
+  nl: () => import("./es.json").then((module) => module.default),
+};
+
+export const useDictionary = (locale: string | number) => {
+  const [dictionary, setDictionary] = useState<Record<string, string>>();
+
+  useEffect(() => {
+    if (locale in dictionaries) {
+      dictionaries[locale]().then((dictionary: Record<string, string>) => {
+        setDictionary(dictionary);
+      });
+    }
+  }, [locale]);
+
+  return dictionary;
+};
