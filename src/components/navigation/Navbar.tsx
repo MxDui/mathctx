5b68605384044d1873a631a61b39a207b6d971ea
David Rivera Morales <driveramorales46@gmail.com>
2023-02-19 22:27:21 -0600

    🚀

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,12 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
     <div className="flex  flex-col">
-      <div className="mb-2 md:border-b py-2">
+      <div className="mb-2 md:border-b py-2 hover:border-yellow-700 transition duration-300">
         <div className="container mx-auto">
           <div className="flex justify-between gap-2 items-center px-2">
-            <h1 className="text-2xl font-bold">MathCTX</h1>
+            <Link href="/" passHref>
+              <h1 className="text-2xl font-bold text-white">MathCTX</h1>
+            </Link>
             <ul className="hidden md:flex">
               <li className="mr-2">
                 <a
